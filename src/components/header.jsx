a970e96ed73b48d13f2afc26e3ced4c56ad96ec3
Nirajan Malla <nirajannitro5@gmail.com>
2023-07-26 23:56:55 +0545

    change in style in navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,51 +6,35 @@ export default function Header() {
     return (
         <div>
             <nav id='nav-bar'>
-                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="max-w-screen-xl flex gap-2 items-center justify-between mx-auto p-4 relative">
                     <Link to="/" className="flex items-center">
                         <img src={Logo} className='w-16 h-16 md:w-24 md:h-24' />
                     </Link>
-                    <div className="flex md:order-2">
 
-                        <div className="relative hidden md:block">
-                            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                </svg>
-                                <span className="sr-only">Search icon</span>
-                            </div>
-                            <input type="text" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-pahelo focus:border-pahelo dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pahelo dark:focus:border-pahelo" placeholder="Search anything..." />
-                        </div>
-                        <button className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" onClick={() => {
-                            setShowNav(!showNav);
-                        }}>
-                            <span className="sr-only">Open main menu</span>
-                            <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="relative block w-full mt-3 md:hidden">
+                    <ul className="absolute border-b-2 md:border-0 pb-4 left-0 top-24 w-full md:relative md:w-fit md:top-0 flex justify-evenly md:p-0 font-medium border-gray-100 bg-gray-50 md:space-x-8 mt-0 md:bg-white dark:bg-black dark:border-gray-700">
+                        <li>
+                            <Link to="/" className="block rounded bg-transparent text-pahelo p-0 md:dark:text-pahelo">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/anotherpage" className="block text-gray-900 rounded md:hover:text-pahelo p-0 dark:hover:text-pahelo dark:text-white dark:hover:bg-gray-70 dark:hover:bg-transparent dark:border-gray-700">Tv Shows</Link>
+                        </li>
+                        <li>
+                            <Link to="/anotherpage" className="block text-gray-900 rounded md:hover:text-pahelo p-0 dark:hover:text-pahelo dark:text-white dark:hover:bg-gray-70 dark:hover:bg-transparent dark:border-gray-700">Movies</Link>
+                        </li>
+                        <li>
+                            <Link to="/anotherpage" className="block text-gray-900 rounded md:hover:text-pahelo p-0 dark:hover:text-pahelo dark:text-white dark:hover:bg-gray-70 dark:hover:bg-transparent dark:border-gray-700">About</Link>
+                        </li>
+                    </ul>
+
+
+                    <div className="relative">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
+                            <span className="sr-only">Search icon</span>
                         </div>
-                        <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-pahelofocus:border-pahelo dark:bg-black dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pahelo dark:focus:border-pahelo" placeholder="Search anything..." />
-                    </div>
-                    <div className={showNav ? "items-center justify-between w-full md:flex md:w-auto md:order-1" : "items-center justify-between w-full hidden md:flex md:w-auto md:order-1"}>
-
-                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-black dark:border-gray-700">
-                            <li>
-                                <Link to="/" className="block py-2 pl-3 pr-4 text-white bg-gray-700 rounded md:bg-transparent md:text-pahelo md:p-0 md:dark:text-pahelo">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/anotherpage" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-pahelo md:p-0 md:dark:hover:text-pahelo dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Tv Shows</Link>
-                            </li>
-                            <li>
-                                <Link to="/anotherpage" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-pahelo md:p-0 dark:text-white md:dark:hover:text-pahelo dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Movies</Link>
-                            </li>
-                        </ul>
+                        <input type="text" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-pahelo focus:border-pahelo dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pahelo dark:focus:border-pahelo" placeholder="Search..." />
                     </div>
                 </div>
 
