5854e40f4dbd0a9112ee22ec9d8e9737808b4468
Nirajan Malla <nirajannitro5@gmail.com>
2023-08-03 23:25:33 +0545

    fetched now playing to demo on homepage with vertical list of movie name

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -8,28 +8,49 @@ import { getApiConfigurations, getGenres } from '../store/rootSlice'
 
 function Root() {
 
-    const dispatch = useDispatch();
-    const url = useSelector((state) => state.home.url.total_pages);
-    console.log(url)
+    // const dispatch = useDispatch();
+    // const url = useSelector((state) => state.home.url.total_pages);
+    // console.log(url)
+
+    // useEffect(() => {
+    //     apiTesting();
+    // }, []);
+
+    // const apiTesting = () => {
+    //     fetchData('movie/now_playing?language=en-US&page=1').then(
+    //         (res) => {
+    //             console.log(res);
+    //             dispatch(getApiConfigurations(res))
+    //         }
+    //     )
+    // }
 
     useEffect(() => {
-        apiTesting();
+        fetchPopular();
     }, []);
 
-    const apiTesting = () => {
-        fetchData('movie/popular').then(
+    const [popularMovies, setPopularMovies] = useState([]);
+
+    const fetchPopular = () => {
+        fetchData('movie/now_playing?language=en-US&page=1').then(
             (res) => {
-                console.log(res);
-                dispatch(getApiConfigurations(res))
+                setPopularMovies(res.results);
             }
         )
     }
+
+
     return (
         <>
             <Header />
-            <div className='max-w-screen-xl mx-auto text-white'>
-                {url}
-            </div>
+            <section className='max-w-screen-xl mx-auto text-white'>
+                <span className='text-pahelo font-black text-3xl'>Now Playing</span>
+                <div className='flex flex-col'>
+                    {popularMovies && popularMovies.map(popularMovie => {
+                        return (<span>{popularMovie.original_title}</span>);
+                    })}
+                </div>
+            </section>
         </>
     )
 }
