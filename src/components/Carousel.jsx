216b68820f97aaf15f925d78720431e2008f1f93
Nirajan Malla <nirajannitro5@gmail.com>
2023-08-17 01:18:47 +0545

    created ref to make slide in carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -6,6 +6,7 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import dateFormat from './dateFormat';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const skeletonItem = () => {
     return (<div className='flex flex-col gap-2 w-32 md:w-36 lg:w-40'>
@@ -16,20 +17,24 @@ const skeletonItem = () => {
 }
 
 export default function Carousel({ data, loading, endpoint }) {
+
+    //will pass this reference to my carousel component
     const carouselBox = useRef();
+
     //importing url from configured redux store
     const { url } = useSelector((state) => state.home);
 
     const navigate = useNavigate();
 
     //writing logic for left and right movement of carousel
+
     const navigation = (dir) => {
         const container = carouselBox.current;
 
         const scrollAmount =
             dir === "left"
-                ? container.scrollLeft - (container.offsetWidth + 20)
-                : container.scrollLeft + (container.offsetWidth + 20);
+                ? container.scrollLeft - container.offsetWidth
+                : container.scrollLeft + container.offsetWidth
 
         container.scrollTo({
             left: scrollAmount,
@@ -37,35 +42,39 @@ export default function Carousel({ data, loading, endpoint }) {
         });
     };
     return (
-        <div>
-            {/* <span
-                onClick={() => navigation("left")}
-            >left</span>
-            <span
-                onClick={() => navigation("right")}
-            >right</span> */}
+        <div className='relative mt-4 md:mb-10'>
 
             {!loading ? (
-                <div className='flex mb-8 gap-2 overflow-x-scroll my-4 no-scrollbar'>
-                    {data?.map((item) => {
-                        //creating posterUrl if found or noposter url from assest incase of error
-                        const posterUrl = item.poster_path ? url.backdrop + item.poster_path : posterNotFound;
-                        return (
-                            <div
-                                key={item.id}
-                                className='flex flex-col gap-2 w-32 md:w-36 lg:w-40'
-                                onClick={() =>
-                                    navigate(`/${item.media_type || endpoint}/${item.id}`)
-                                }>
-                                <div className='relative w-32 md:w-36 lg:w-40 aspect-[poster] h-60'>
-                                    <LazyLoadImage alt={item.title} src={posterUrl} className='object-cover w-full h-full object-center' />
+                <>
+                    <span
+                        onClick={() => navigation("left")}
+                        className='hidden md:inline absolute left-0 -bottom-10 z-50 cursor-pointer text-slate-300/70 hover:text-white'
+                    ><FaChevronLeft /></span>
+                    <span
+                        onClick={() => navigation("right")}
+                        className='hidden md:inline absolute right-0 -bottom-10 z-50 cursor-pointer text-slate-300/70 hover:text-white'
+                    ><FaChevronRight /></span>
+                    <div className='flex mb-8 gap-2 overflow-x-scroll my-4 no-scrollbar' ref={carouselBox}>
+                        {data?.map((item) => {
+                            //creating posterUrl if found or noposter url from assest incase of error
+                            const posterUrl = item.poster_path ? url.backdrop + item.poster_path : posterNotFound;
+                            return (
+                                <div
+                                    key={item.id}
+                                    className='flex flex-col gap-2 w-32 md:w-36 lg:w-40'
+                                    onClick={() =>
+                                        navigate(`/${item.media_type || endpoint}/${item.id}`)
+                                    }>
+                                    <div className='relative w-32 md:w-36 lg:w-40 aspect-[poster] h-60'>
+                                        <LazyLoadImage alt={item.title} src={posterUrl} className='object-cover w-full h-full object-center' />
+                                    </div>
+                                    <span className='text-ellipsis overflow-x-hidden whitespace-nowrap'>{item.title || item.name}</span>
+                                    <span className='text-xs text-slate-200'>{dateFormat(item.release_date)}</span>
                                 </div>
-                                <span className='text-ellipsis overflow-x-hidden whitespace-nowrap'>{item.title || item.name}</span>
-                                <span className='text-xs text-slate-200'>{dateFormat(item.release_date)}</span>
-                            </div>
-                        )
-                    })}
-                </div>
+                            )
+                        })}
+                    </div>
+                </>
             ) : (<div className='flex mb-8 gap-2 overflow-x-scroll my-4 no-scrollbar'>
                 {skeletonItem()}
                 {skeletonItem()}
