525a4cecbcbc456de07b762df4b638825acc6393
Nirajan Malla <nirajannitro5@gmail.com>
2023-08-18 11:41:23 +0545

    created a new section and implemented toogle fetch in all section required

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -40,32 +40,63 @@ function Root() {
     ]
 
     const popular = useFetch(`${popularOpt}/popular`);
+
     const playing = useFetch("movie/now_playing");
-    const toprated = useFetch("movie/top_rated");
+
+    const [topOpt, setTopOpt] = useState("movie");
+
+    const tOpt = [
+        { name: "Movie", value: "movie" },
+        { name: "Tv Shows", value: "tv" }
+    ]
+    const toprated = useFetch(`${topOpt}/top_rated`);
+
+    const [trendingOpt, setTrendingOpt] = useState("day");
+
+    const trOpt = [
+        { name: "Today", value: "day" },
+        { name: "This Week", value: "week" }
+    ]
+    const trending = useFetch(`trending/all/${trendingOpt}`);
+
 
 
 
     return (
         <div className='max-w-screen-xl mx-auto text-white sm:px-4 px-2'>
+
             <section className='mb:8 md:mb-24 mt-16 md:mt-0'>
+                <div>
+                    <span className='text-pahelo font-black text-3xl'>Trending</span>
+                    <OnOff opt={trOpt} stateChanger={setTrendingOpt} />
+                </div>
+                <Carousel data={trending?.data?.results} loading={trending?.loading} />
+            </section>
+
+            <section className='mb:8 md:mb-24'>
                 <span className='text-pahelo font-black text-3xl'>Now Playing</span>
                 <Carousel data={playing?.data?.results} loading={playing?.loading} />
-                <Outlet />
             </section>
+
             <section className='mb-8 md:mb-24'>
                 <div>
                     <span className='text-pahelo font-black text-3xl'>Popular</span>
                     <OnOff opt={pOpt} stateChanger={setPopularOpt} />
                 </div>
                 <Carousel data={popular?.data?.results} loading={popular?.loading} />
-                <Outlet />
             </section>
+
             <section className='mb-8 md:mb-24'>
-                <span className='text-pahelo font-black text-3xl'>Top rated</span>
+                <div>
+                    <span className='text-pahelo font-black text-3xl'>Top rated</span>
+                    <OnOff opt={tOpt} stateChanger={setTopOpt} />
+                </div>
                 <Carousel data={toprated?.data?.results} loading={toprated?.loading} />
-                <Outlet />
             </section>
+
+            <Outlet />
         </div>
+
     )
 }
 
