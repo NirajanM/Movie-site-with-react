7aed8c8c423c51b364d21f69e0a10471a9199949
Nirajan Malla <nirajannitro5@gmail.com>
2023-08-24 17:32:25 +0545

    loading skeleton when fetching is incomplete in detail section of movies

--- src/components/DetailSection.jsx
+++ src/components/DetailSection.jsx
@@ -3,16 +3,25 @@ import { BiCameraMovie } from "react-icons/bi";
 import useFetch from "../hooks/useFetch";
 import ReactPlayer from "react-player";
 import { useState } from "react";
+import Skeleton from "react-loading-skeleton";
 
-export default function DetailSection({ mediaType, id, data }) {
+export default function DetailSection({ mediaType, id, data, loading }) {
     const { data: videos } = useFetch(`${mediaType}/${id}/videos`);
     const ytUrl = "https://www.youtube.com/watch?v=";
     const [trailer, setTrailer] = useState(false);
 
     return (
         <>
+            {trailer ?
+                <div
+                    className="flex fixed h-screen bg-slate-950/70 w-screen top-0 left-0 justify-center items-center z-50"
+                    onClick={() => { setTrailer(false) }}
+                >
+                    <ReactPlayer url={ytUrl + videos?.results?.find((v) => v.type === "Trailer").key} controls={true} playing={true} />
+                </div> : null
+            }
             <div className='md:basis-3/4 flex flex-col gap-2'>
-                {data &&
+                {loading ? <Skeleton width={100} /> : data &&
                     <div className='flex items-center gap-2'>
                         <ReactStars
                             count={5}
@@ -25,25 +34,48 @@ export default function DetailSection({ mediaType, id, data }) {
                         <span>{Math.round(data?.vote_average * 10) / 10}/10</span>
                     </div>
                 }
-                <span className='text-xl md:text-4xl lg:text-6xl font-bold md:font-black text-pahelo'>{data?.title?.toUpperCase()}</span>
-                <span className='text-xl md:text-4xl lg:text-6xl font-bold md:font-black text-pahelo'>{data?.original_name?.toUpperCase()}</span>
-                <span className='text-slate-300/70 italic font-normal lg:text-xl'>{data?.tagline}</span>
+                {
+                    loading
+                        ?
+                        <>
+                            <Skeleton width={400} height={80} />
+                            <Skeleton width={200} height={23} />
+
+                        </>
+                        :
+                        <>
+                            <span
+                                className='text-xl md:text-4xl lg:text-6xl font-bold md:font-black text-pahelo'
+                            >{data?.title?.toUpperCase()}</span>
+
+                            <span
+                                className='text-xl md:text-4xl lg:text-6xl font-bold md:font-black text-pahelo'
+                            >{data?.original_name?.toUpperCase()}</span>
+                            <span className='text-slate-300/70 italic font-normal lg:text-xl'>{data?.tagline}</span>
+                        </>
+                }
+
+
                 <div>
                     <span></span>
                     <span
                         className='inline-flex items-center px-2 bg-red-700 gap-1 cursor-pointer'
                         onClick={() => { setTrailer(true) }}
                     ><BiCameraMovie /> TRAILER</span>
-                    {trailer ?
-                        <div
-                            className="flex fixed h-screen bg-slate-950/70 w-screen top-0 left-0 justify-center items-center z-50"
-                            onClick={() => { setTrailer(false) }}
-                        >
-                            <ReactPlayer url={ytUrl + videos?.results?.find((v) => v.type === "Trailer").key} controls={true} playing={true} />
-                        </div> : null
-                    }
                 </div>
-                <p className='lg:text-md'>{data?.overview}</p>
+
+                {
+                    loading
+                        ?
+                        <>
+                            <Skeleton width="100%" height={18} />
+                            <Skeleton width="100%" height={18} />
+                            <Skeleton width="50%" height={18} />
+                        </>
+                        :
+                        <p className='lg:text-md'>{data?.overview}</p>
+
+                }
             </div>
         </>
     )
--- src/routes/SelectedCinema.jsx
+++ src/routes/SelectedCinema.jsx
@@ -4,10 +4,11 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { useSelector } from 'react-redux';
 import CastSection from '../components/CastSection';
 import DetailSection from '../components/DetailSection';
+import Skeleton from 'react-loading-skeleton';
 
 export default function SelectedCinema() {
     const { mediaType, id } = useParams();
-    const { data } = useFetch(`${mediaType}/${id}`);
+    const { data, loading } = useFetch(`${mediaType}/${id}`);
     let { url } = useSelector((state) => state.home);
     url = url.backdrop ? url.backdrop : "https://image.tmdb.org/t/p/original";
     const backdrop = url + data?.backdrop_path;
@@ -18,11 +19,17 @@ export default function SelectedCinema() {
         <div className='text-white'>
             <LazyLoadImage alt={data?.title} src={backdrop} className='absolute top-0 -z-10 object-cover w-screen h-screen object-center opacity-10' />
             <div className='max-w-screen-xl mx-auto sm:px-4 px-2 '>
-                <section className='w-full mt-10 md:mt-3 p-4 md:p-0 flex flex-col md:flex-row gap-2 md:gap-10 lg:gap-14 xl:gap-18 min-h-50 relative'>
+                <section className='w-full mt-10 md:mt-3 p-4 md:p-0 flex flex-col md:flex-row gap-2 md:gap-10 lg:gap-14 xl:gap-18 relative'>
                     <div className='md:basis-1/4 md:min-w-60'>
-                        <LazyLoadImage src={posterpath} alt={data?.title} className='md:rounded-md' />
+                        {loading
+                            ?
+                            <Skeleton className='aspect-poster' />
+                            :
+                            <LazyLoadImage src={posterpath} alt={data?.title} className='md:rounded-md' />
+                        }
+
                     </div>
-                    <DetailSection mediaType={mediaType} id={id} data={data} />
+                    <DetailSection mediaType={mediaType} id={id} data={data} loading={loading} />
                 </section>
                 <section className='mt-10 mb-20'>
                     <span className='text-lg sm:text-xl md:text-3xl font-semibold text-slate-200/80'>Cast</span>
