23e314705c56770baeb6ab6193236f2a8361b11a
Nirajan Malla <nirajannitro5@gmail.com>
2023-08-27 13:30:07 +0545

    blocking popupads in iframe

--- src/routes/Play.jsx
+++ src/routes/Play.jsx
@@ -4,7 +4,7 @@ export default function Play() {
     const { id } = useParams();
     return (
         <div className="text-xl text-white w-screen h-screen mt-16 max-w-screen-xl mx-auto sm:px-4 px-2 mb-20">
-            <iframe src={`https://autoembed.to/movie/tmdb/${id}`} width="100%" height="100%" />
+            <iframe name="iframe1" id="iframe1" src={`https://autoembed.to/movie/tmdb/${id}`} width="100%" height="100%" sandbox="allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation" />
         </div>
     )
 }
