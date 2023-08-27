236b0ad17a8c524a61b5016a6f6cbccbd53a4cf9
Nirajan Malla <nirajannitro5@gmail.com>
2023-08-27 23:48:08 +0545

    designed genre menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,24 @@
 import { BiMoviePlay } from "react-icons/bi";
-import { RiMovieLine } from "react-icons/ri";
+import { RiMovie2Line } from "react-icons/ri";
 import { RxAvatar } from "react-icons/rx";
 import { BsQuestionOctagon } from "react-icons/bs";
+import { GoSingleSelect } from "react-icons/go";
+
 export default function Menu() {
     return (
         <div className=" top-0 text-white w-1/6 border-r border-gray-600 hidden md:inline pt-16 pr-5 h-screen sticky">
             <ul className="flex-col justify-start items-center space-y-14 text-lg text-gray-400 font-medium">
-                <li className="list"><BiMoviePlay className="text-2xl" />Genre</li>
-                <li className="list"><RiMovieLine className="text-2xl" />Watchlist</li>
+                <li>
+                    <div className="list">
+                        <BiMoviePlay className="text-2xl" />
+                        <div className="flex flex-col">
+                            <span className="flex gap-[10px] items-center">Genre <GoSingleSelect /></span>
+                            <span className="text-xs text-slate-100 pl-1 font-light">- default</span>
+                        </div>
+                    </div>
+
+                </li>
+                <li className="list"><RiMovie2Line className="text-2xl" />Watchlist</li>
                 <li className="list"><RxAvatar className="text-2xl" />About</li>
                 <li className="list"><BsQuestionOctagon className="text-2xl" />FAQs</li>
             </ul>
