e2badc4b0d2188fffaea9213b2a0050daee6935c
Nirajan Malla <nirajannitro5@gmail.com>
2023-09-04 23:56:03 +0545

    refactored to app.jsx for calling redux store at initial load rather than load of root.jsx so that image url is properly updated

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import Root from "./routes/Root";
+import Anotherpage from './routes/Anotherpage';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
+import SelectedCinema from './routes/SelectedCinema';
+import PageNotFound from './routes/PageNotFound';
+import RootLayout from './layouts/rootLayout';
+import SearchResult from './routes/SearchResult';
+import Play from './routes/Play';
+import Tvshow from './routes/Tvshow';
+import Movies from './routes/Movies';
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchData } from "./utils/api";
+import { getApiConfigurations } from "./store/rootSlice";
+
+export default function App() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetchConfiguration();
+    },);
+
+    const fetchConfiguration = () => {
+        fetchData('/configuration').then(
+            (res) => {
+                //extracting necessary url from config response
+                const url = {
+                    backdrop: res.images.secure_base_url + "original",
+                    poster: res.images.secure_base_url + "original",
+                    profile: res.images.secure_base_url + "original",
+                }
+
+                //setting it for global use
+                dispatch(getApiConfigurations(url));
+            }
+        )
+    }
+
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path='/' element={<RootLayout />}>
+                <Route index element={<Root />} />
+                <Route path=':mediaType/:id' element={<SelectedCinema />} />
+                <Route path=':mediaType/:id/play' element={<Play />} />
+                <Route path='search/:query' element={<SearchResult />} />
+                <Route path='anotherpage' element={<Anotherpage />} />
+                <Route path='tvshow' element={<Tvshow />} />
+                <Route path='movies' element={<Movies />} />
+                <Route path='*' element={<PageNotFound />} />
+            </Route>
+        ));
+    return (
+        <RouterProvider router={router} />
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,15 @@
 import ReactDOM from 'react-dom/client'
-import Root from "./routes/Root";
-import Anotherpage from './routes/Anotherpage';
 import './index.css'
 import { store } from './store/store';
 import { Provider } from 'react-redux';
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import SelectedCinema from './routes/SelectedCinema';
-import PageNotFound from './routes/PageNotFound';
-import RootLayout from './layouts/rootLayout';
-import SearchResult from './routes/SearchResult';
-import Play from './routes/Play';
-import Tvshow from './routes/Tvshow';
-import Movies from './routes/Movies';
+import App from './App';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
-      <Route index element={<Root />} />
-      <Route path=':mediaType/:id' element={<SelectedCinema />} />
-      <Route path=':mediaType/:id/play' element={<Play />} />
-      <Route path='search/:query' element={<SearchResult />} />
-      <Route path='anotherpage' element={<Anotherpage />} />
-      <Route path='tvshow' element={<Tvshow />} />
-      <Route path='movies' element={<Movies />} />
-      <Route path='*' element={<PageNotFound />} />
-    </Route>
-  ));
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <App />
   </Provider>
 )
 
