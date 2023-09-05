949665293670b5ba7ed9beb3149bfce6045674a6
Nirajan Malla <nirajannitro5@gmail.com>
2023-09-06 00:23:22 +0545

    list refresh based on genre selected

--- src/routes/Movies.jsx
+++ src/routes/Movies.jsx
@@ -1,15 +1,17 @@
+import { useSelector } from "react-redux";
 import Menu from "../components/Menu";
 import Repeater from "../components/Repeater";
 import useFetch from "../hooks/useFetch";
 
 
 export default function Movies() {
-    const trending = useFetch(`discover/movie`);
+    const { movieGenre } = useSelector((state) => state.home);
+    const movies = useFetch(`discover/movie?with_genres=${movieGenre.id}`);
     return (
         <div className='flex max-w-screen-xl mx-auto'>
             <Menu />
             <div className='w-full md:w-5/6 md:pl-9 text-white px-2'>
-                <Repeater data={trending?.data?.results} loading={trending?.loading} endpoint={"movie"} />
+                <Repeater data={movies?.data?.results} loading={movies?.loading} endpoint={"movie"} />
             </div>
         </div>
     )
--- src/routes/Tvshow.jsx
+++ src/routes/Tvshow.jsx
@@ -1,15 +1,17 @@
+import { useSelector } from "react-redux";
 import Menu from "../components/Menu";
 import Repeater from "../components/Repeater";
 import useFetch from "../hooks/useFetch";
 
 
 export default function Tvshow() {
-    const trending = useFetch(`discover/tv`);
+    const { tvGenre } = useSelector((state) => state.home);
+    const tvshow = useFetch(`discover/tv?with_genres=${tvGenre.id}`);
     return (
         <div className='flex max-w-screen-xl mx-auto'>
             <Menu />
             <div className='w-full md:w-5/6 md:px-9 text-white px-2'>
-                <Repeater data={trending?.data?.results} loading={trending?.loading} endpoint={"tv"} />
+                <Repeater data={tvshow?.data?.results} loading={tvshow?.loading} endpoint={"tv"} />
             </div>
         </div>
     )
