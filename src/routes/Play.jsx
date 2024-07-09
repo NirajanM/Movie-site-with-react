0371845304abda58d1ab646068e1a6b237597de5
Nirajan Malla <nirajannitro5@gmail.com>
2024-07-09 16:54:46 +0545

    working on search feature, completed search part, remaining to implement usequery

--- src/routes/Play.jsx
+++ src/routes/Play.jsx
@@ -1,10 +1,17 @@
 import { useParams } from "react-router-dom";
 
 export default function Play() {
-    const { id } = useParams();
-    return (
-        <div className="text-xl text-white w-screen h-screen mt-16 max-w-screen-xl mx-auto sm:px-4 px-2 mb-20">
-            <iframe name="iframe1" id="iframe1" src={`https://autoembed.to/movie/tmdb/${id}`} width="100%" height="100%" allowFullScreen />
-        </div>
-    )
+  const { id } = useParams();
+  return (
+    <div className="text-xl text-white w-screen h-screen mt-16 max-w-screen-xl mx-auto sm:px-4 px-2 mb-20">
+      <iframe
+        name="iframe1"
+        id="iframe1"
+        src={`https://embed.smashystream.com/playere.php?tmdb=${id}`}
+        width="100%"
+        height="100%"
+        allowFullScreen
+      />
+    </div>
+  );
 }
--- src/routes/SearchResult.jsx
+++ src/routes/SearchResult.jsx
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom";
+import SearchRepeater from "../components/SearchRepeater";
+
 export default function SearchResult() {
-    return (
-        <div className='text-white'>
-            Search Result
-        </div>
-    )
+  const { query } = useParams();
+  return (
+    <div className="flex max-w-screen-xl mx-auto">
+      <div className="w-full md:w-5/6 md:pl-9 text-white px-2">
+        <SearchRepeater keyword={query} />
+      </div>
+    </div>
+  );
 }
