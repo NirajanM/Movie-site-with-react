9bcdf5df8f7fd194c82f2ba3abbedef6d040e181
Nirajan Malla <nirajannitro5@gmail.com>
2024-07-10 11:42:11 +0545

    handling warnings typos

--- src/components/SearchRepeater.jsx
+++ src/components/SearchRepeater.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import posterNotFound from "../assets/posterNotFound.png";
