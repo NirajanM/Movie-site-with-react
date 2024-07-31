cb0435f1962cf3d03abcd900e3f6e97db13b2da2
Nirajan Malla <nirajannitro5@gmail.com>
2024-07-31 14:53:41 +0545

    used see more button with tanstack query instead of infinite scroll, to reduce api calls + responsiveness work

--- src/components/Repeater.jsx
+++ src/components/Repeater.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import posterNotFound from "../assets/posterNotFound.png";
@@ -8,6 +8,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 import dateFormat from "./dateFormat";
 import { fetchData } from "../utils/api";
 import { AiFillStar } from "react-icons/ai";
+import { useQuery } from "@tanstack/react-query";
 
 const skeletonItem = () => {
   return (
@@ -32,77 +33,91 @@ export default function Repeater({ genre, endpoint }) {
   const { url } = useSelector((state) => state.home);
   const [page, setPage] = useState(1);
   const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
 
   let fetchingUrl =
     `discover/${endpoint}?page=${page}` +
     (genre ? `&with_genres=${genre}` : "");
 
-  useEffect(() => {
-    setLoading(true);
-    setError(null);
-
-    fetchData(fetchingUrl)
-      .then((res) => {
-        setLoading(false);
-        setItems(res.results);
-      })
-      .catch((err) => {
-        setLoading(false);
-        setError(err);
-      });
-  }, [genre]);
-
-  useEffect(() => {
-    page !== 1 &&
-      fetchData(fetchingUrl)
-        .then((res) => {
-          setLoading(false);
-          setItems((prevItems) => [...prevItems, ...res.results]);
-        })
-        .catch((err) => {
-          setLoading(false);
-          setError(err);
-        });
-  }, [page]);
-
-  // Intersection Observer callback function
-  const handleIntersection = (entries) => {
-    const [entry] = entries;
-    if (entry.isIntersecting) {
-      // Scrollbar has reached the bottom
-      setPage((prevPage) => prevPage + 1);
+  const { data, error, isLoading } = useQuery(
+    ["items", page, genre, endpoint],
+    () => fetchData(fetchingUrl),
+    {
+      keepPreviousData: true,
+      onSuccess: (data) => {
+        setItems((prevItems) => [...prevItems, ...data.results]);
+      },
     }
+  );
+
+  const loadMoreItems = () => {
+    setPage((prevPage) => prevPage + 1);
   };
 
-  useEffect(() => {
-    // Create an Intersection Observer
-    const observer = new IntersectionObserver(handleIntersection, {
-      root: null,
-      rootMargin: "0px",
-      threshold: 1, // Adjust this threshold as needed
-    });
+  // const [loading, setLoading] = useState(null);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setError(null);
 
-    // Observe the last item in the list
-    if (carouselBox.current && items.length > 0) {
-      observer.observe(carouselBox.current.lastChild);
-    }
+  //   fetchData(fetchingUrl)
+  //     .then((res) => {
+  //       setLoading(false);
+  //       setItems(res.results);
+  //     })
+  //     .catch((err) => {
+  //       setLoading(false);
+  //       setError(err);
+  //     });
+  // }, [genre]);
 
-    return () => {
-      // Disconnect the Intersection Observer when the component unmounts
-      observer.disconnect();
-    };
-  }, [items]);
+  // useEffect(() => {
+  //   page !== 1 &&
+  //     fetchData(fetchingUrl)
+  //       .then((res) => {
+  //         setLoading(false);
+  //         setItems((prevItems) => [...prevItems, ...res.results]);
+  //       })
+  //       .catch((err) => {
+  //         setLoading(false);
+  //         setError(err);
+  //       });
+  // }, [page]);
+
+  // // Intersection Observer callback function
+  // const handleIntersection = (entries) => {
+  //   const [entry] = entries;
+  //   if (entry.isIntersecting) {
+  //     // Scrollbar has reached the bottom
+  //     setPage((prevPage) => prevPage + 1);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   // Create an Intersection Observer
+  //   const observer = new IntersectionObserver(handleIntersection, {
+  //     root: null,
+  //     rootMargin: "0px",
+  //     threshold: 0, // Adjust this threshold as needed
+  //   });
+
+  //   // Observe the last item in the list
+  //   if (carouselBox.current && items.length > 0) {
+  //     observer.observe(carouselBox.current.lastChild);
+  //   }
+
+  //   return () => {
+  //     // Disconnect the Intersection Observer when the component unmounts
+  //     observer.disconnect();
+  //   };
+  // }, [items]);
 
   return (
-    <div className="relative my-16 md:my-6">
+    <div className="relative mt-16 md:mt-6 min-h-screen">
       {error ? (
         error
-      ) : !loading ? (
+      ) : !isLoading ? (
         <>
           <div
-            className=" grid mx-auto my-4 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3 md:gap-7 xl:gap-10 "
+            className=" grid mx-auto my-4 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 md:gap-7 min-h-screen"
             ref={carouselBox}
           >
             {items.map((item) => {
@@ -114,9 +129,10 @@ export default function Repeater({ genre, endpoint }) {
                 <div
                   key={item.id}
                   className="flex flex-col gap-2 group w-full cursor-pointer"
-                  onClick={() =>
-                    navigate(`/${item.media_type || endpoint}/${item.id}`)
-                  }
+                  onClick={() => {
+                    navigate(`/${item.media_type || endpoint}/${item.id}`);
+                    window.scrollTo(0, 0);
+                  }}
                 >
                   <div className="relative aspect-poster min-w-[128px] md:min-w-[144px] lg:min-w-[160px]">
                     <LazyLoadImage
@@ -140,6 +156,14 @@ export default function Repeater({ genre, endpoint }) {
                 </div>
               );
             })}
+          </div>
+          <div className="flex justify-center items-center pt-5 pb-10 w-full">
+            <button
+              onClick={loadMoreItems}
+              className="mt-4 px-4 py-2 border border-pahelo hover:bg-gray-500/40 text-pahelo rounded"
+            >
+              {isLoading ? "loading..." : "Load More"}
+            </button>
           </div>
         </>
       ) : (
