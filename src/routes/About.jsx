dd28fd096251efd8b05adead305240a5420d7c9f
Nirajan Malla <nirajannitro5@gmail.com>
2024-08-04 14:10:53 +0545

    single img element

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,4 +1,3 @@
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import flyerurl from "../assets/Lets watch movie.webp";
 import Menu from "../components/Menu";
 export default function About() {
@@ -7,11 +6,12 @@ export default function About() {
       <div className="flex max-w-screen-xl mx-auto">
         <Menu />
         <div className="w-full md:pl-9 text-white mt-10 py-4 md:mt-0 flex justify-center items-start flex-col gap-4 md:gap-8 lg:gap-12">
-          <div className="sm:w-11/12 md:w-4/5 lg:w-2/3">
-            <LazyLoadImage
+          <div className="sm:w-11/12 md:w-4/5 lg:w-2/3 aspect-auto">
+            <img
               alt="lets watch movie flyer"
               src={flyerurl}
-              className="object-cover w-full h-auto object-center "
+              width="100%"
+              height="100%"
             />
           </div>
 
