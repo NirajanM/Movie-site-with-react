21b001b2764d78909b4c4011ad22bb7f8b23c7b8
Nirajan Malla <nirajannitro5@gmail.com>
2024-08-12 16:44:27 +0545

    using tabs instead of selector seem logical and so is done

--- src/routes/SearchResult.jsx
+++ src/routes/SearchResult.jsx
@@ -2,38 +2,52 @@ import { useParams } from "react-router-dom";
 import SearchRepeater from "../components/SearchRepeater";
 import Menu from "../components/Menu";
 import { useState } from "react";
-import {
-  Select,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectItem,
-} from "@/components/ui/select";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 export default function SearchResult() {
   const { query } = useParams();
   const [type, setType] = useState("multi");
+
   return (
     <div className="flex max-w-screen-xl mx-auto">
       <Menu />
       <div className="w-full lg:w-5/6 md:px-4 text-white mx-auto mt-10 md:mt-0">
         <section
           id="filters"
-          className="flex flex-col mt-4 lg:mt-6 mb-2 lg:mb-4 gap-2 border-b border-white/10 pb-8 "
+          className="flex flex-col mt-4 lg:mt-6 mb-2 lg:mb-4 gap-2 pb-8 "
         >
-          <h3 className="text-base lg:text-base text-white/50">Select Type</h3>
-          <Select value={type} onValueChange={setType}>
-            <SelectTrigger className="w-[180px] ring-0 focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0">
-              <SelectValue placeholder="Type" />
-            </SelectTrigger>
-            <SelectContent className="ring-0">
-              <SelectItem value="movie">Movie</SelectItem>
-              <SelectItem value="tv">TV shows</SelectItem>
-              <SelectItem value="multi">Both</SelectItem>
-            </SelectContent>
-          </Select>
+          <Tabs defaultValue={type} onValueChange={setType}>
+            <TabsList className="bg-transparent dark:bg-transparent px-0 gap-2">
+              <TabsTrigger
+                value="movie"
+                className="hover:bg-slate-800 hover:text-slate-300"
+              >
+                Movies
+              </TabsTrigger>
+              <TabsTrigger
+                value="tv"
+                className="hover:bg-slate-800 hover:text-slate-300"
+              >
+                TV shows
+              </TabsTrigger>
+              <TabsTrigger
+                value="multi"
+                className="hover:bg-slate-800 hover:text-slate-300"
+              >
+                All
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="movie">
+              <SearchRepeater keyword={query} type="movie" />
+            </TabsContent>
+            <TabsContent value="tv">
+              <SearchRepeater keyword={query} type="tv" />
+            </TabsContent>
+            <TabsContent value="multi">
+              <SearchRepeater keyword={query} type="multi" />
+            </TabsContent>
+          </Tabs>
         </section>
-        <SearchRepeater keyword={query} type={type} />
       </div>
     </div>
   );
